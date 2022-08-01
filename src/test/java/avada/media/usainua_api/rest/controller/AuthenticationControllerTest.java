ea5e1ba403a4b84df3aa19dddaff92380a17b3e0
black <antariuz@gmail.com>
2022-08-01 16:32:17 +0300

    Path fixes

--- src/test/java/avada/media/usainua_api/rest/controller/AuthenticationControllerTest.java
+++ src/test/java/avada/media/usainua_api/rest/controller/AuthenticationControllerTest.java
@@ -96,6 +96,7 @@ class AuthenticationControllerTest {
     void refreshTokenWithNoTokenInHeader() throws Exception {
         this.mockMvc.perform(get("/auth/token/refresh"))
                 .andExpect(status().isForbidden())
+
                 .andExpect(status().reason(containsString("Refresh token has not been found or wrong")))
                 .andExpect(unauthenticated());
     }
